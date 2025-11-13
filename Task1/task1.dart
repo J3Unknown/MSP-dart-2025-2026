d38b03a6ce6464c1bd1068ca4f876cf5e720834d
Human700 <human.atmoon@gmail.com>
2025-11-13 14:50:18 +0200

    Task 1

---
+++ Task1/task1.dart
@@ -0,0 +1,20 @@
+//* Problem 1
+String rps(String p1, String p2) {
+  //! write your code here
+
+  return " ";
+}
+
+//* Problem 2
+int getCount(String inputStr) {
+  //! write your code here
+
+  return -1;
+}
+
+//* problem 3 (optional)
+int duplicateCount(String text) {
+  //! write your code here
+
+  return -1;
+}
