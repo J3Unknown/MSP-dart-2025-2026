6ba558e2103b582f34aeb49aa36d7fe895bc8356
AhmedMK <152314340+ZVAXEROWS@users.noreply.github.com>
2025-11-14 20:34:32 +0200

    Done

---
+++ Task1/Khalaf/task1.dart
@@ -0,0 +1,76 @@
+//* Problem 1
+String rps(String p1, String p2) {
+
+  int sub = p1.length - p2.length;
+
+  if(sub == 0){
+    print("Draw!");
+
+  }else if(sub.abs() == 4){
+    if(sub < 0){
+      print("Player 1 won!");
+    }else{
+      print("Player 2 won!");
+    }
+  }else {
+    if(sub > 0){
+      print("Player 1 won!");
+    }else{
+      print("Player 2 won!");
+    }  
+  }
+
+
+  return " ";
+}
+
+//* Problem 2
+int getCount(String inputStr) {
+  
+    int vowels = 0;
+  int spaces = 0;
+
+  inputStr = inputStr.toLowerCase();
+
+  for(String a in inputStr.split('')){
+    if(a == ' '){
+      spaces++;
+    }else if (a == 'a' || a =='e' || a =='i' || a=='o' || a=='u'){
+      vowels++;
+    }
+  }
+
+  print("$inputStr -> $vowels");
+  if(spaces == 0){
+    print("No Spaces In The String");
+  }else{
+    print("$spaces Spaces");
+  }
+
+  return -1;
+}
+
+//* problem 3 (optional)
+int duplicateCount(String text) {
+
+  Map <String , int> dublicated = {};
+
+  text = text.toLowerCase();
+  int result=0;
+
+  for(String a in text.split('')){
+    if(dublicated[a] == null){
+      dublicated[a] = 0;
+    }else{
+      dublicated[a] = dublicated[a]! + 1;
+    }
+  }
+
+  dublicated.removeWhere((key, value) => value ==0);
+
+  result = dublicated.length;
+
+  print("\"$text\"  => $result");
+
+  return -1;
+}
